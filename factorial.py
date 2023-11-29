4eff7dfc8d9dc552c4eb54dcb767368b416fd600
mrendyg <andyg.oyarzo@gmail.com>
2023-11-29 12:34:45 -0300

    Factorial

--- factorial.py
+++ factorial.py
@@ -1,4 +1,9 @@
 #calculo factorial
+acumulador = 1
 
 numero_ingresado = int(input("Ingresa un numero:\n"))
 
+for i in range (1, numero_ingresado+1):
+    acumulador = acumulador * i
+ 
+print("El factorial de", numero_ingresado, "es:\n", acumulador)
