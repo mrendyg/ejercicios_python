5a79a0e7320bbd0b674d2d7ccd47c697eafa22c6
mrendyg <andyg.oyarzo@gmail.com>
2024-01-10 12:03:27 -0300

    mejora en generador de password

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,10 +1,10 @@
 import random
 
-chars = "abcdefghijklmnopqrstuvwxyz1234567890,.-;:_{[]}´+*?¡'¿"
+chars = "ABCDEFGHIJKLMNOPQRSTVWXYZabcdefghijklmnopqrstuvwxyz1234567890,.-;:_{[]}´+*?¡'¿"
 
 password = ""
 
-for i in range(16):
+for i in range(20):
     password += random.choice(chars)
 
 print(f"Your password is: {password}")
